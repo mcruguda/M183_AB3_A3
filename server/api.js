54f5a4293662799ea8b3ceccefe8aa194bbafac6
Cyrill Augustiny <c.augustiny@gmail.com>
2024-10-31 11:36:25 +0100

    ZOD Validation
    Email and password validation (password only validated to have 10+ chars)

--- server/api.js
+++ server/api.js
@@ -1,8 +1,19 @@
+const { z } = require("zod");
 const initializeAPI = async (app) => {
   app.post("/api/login", login);
 };
 
+const inputScheme = z.object({
+  username: z.string().email().min(1),
+  password: z.string().min(10),
+});
+
 const login = async (req, res) => {
+  const input = inputScheme.safeParse(req.body);
+  if (input) {
+    return res.status(400).send(input);
+  }
+
   const { username, password } = req.body;
 
   const answer = `
